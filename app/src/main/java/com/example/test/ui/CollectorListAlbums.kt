38265c179ce4303d775756d3f959d37e34451485
Fernando Garzon <of.garzon2662@uniandes.edu.co>
2023-11-12 14:15:08 -0800

    Adelanto de Listar Albumes Colector

--- app/src/main/java/com/example/test/ui/CollectorListAlbums.kt
+++ app/src/main/java/com/example/test/ui/CollectorListAlbums.kt
@@ -1,34 +1,17 @@
 package com.example.test.ui
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.test.R
 import com.example.test.databinding.ActivityCollectorListAlbumsBinding
-import android.content.Intent
-import android.view.View
-import android.widget.AdapterView
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.test.common.validateDate
-import com.example.test.common.validateImage
-import com.example.test.common.validateSpinner
-import com.example.test.common.validateValue
-import com.example.test.databinding.ActivityCollectorAddAlbumBinding
 import com.example.test.ui.adapters.AlbumAdapter
-import com.example.test.viewmodel.CollectorAddAlbumViewModel
 import com.example.test.viewmodel.CollectorListAlbumViewModel
 
 class CollectorListAlbums : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityCollectorListAlbumsBinding
     private lateinit var albumAdapter: AlbumAdapter // Declare the adapter
 
