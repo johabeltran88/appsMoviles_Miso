5eada346dcf172a239a57794d83ed57460556eaf
Fernando Garzon <of.garzon2662@uniandes.edu.co>
2023-11-29 16:49:17 -0800

    Errores de accesibilidad corregidos

--- app/src/main/java/com/example/test/ui/VisitorHomeActivity.kt
+++ app/src/main/java/com/example/test/ui/VisitorHomeActivity.kt
@@ -5,7 +5,6 @@ import android.graphics.PorterDuff
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import androidx.core.content.ContextCompat
 import com.example.test.R
 import com.example.test.databinding.ActivityVisitorHomeBinding
